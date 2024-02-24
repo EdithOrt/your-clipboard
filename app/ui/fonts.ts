abd0fc2deb0768dd2641ef0ae513cbf14a260b54
EdithOrt <eoc.diseno@gmail.com>
2024-02-24 13:40:51 -0600

    [UPDATE] add fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Manjari, Roboto } from "next/font/google";
+
+export const manjari = Manjari({ subsets: ["latin"], weight: "400" });
+export const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"] });
